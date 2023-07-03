7a23e10f7539332d6d7cfb8cb6627ea28c4b5c66
firesquid06 <jdeiss06@gmail.com>
2023-07-03 14:13:09 -0500

    add logging in with otp

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		port: 8008
-	}
+  plugins: [svelte()],
+  server: {
+    port: 8008,
+  },
 });
