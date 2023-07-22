1043dafa4253351282aeeeef8836010c5ea271da
firesquid <jdeiss06@gmail.com>
2023-07-22 15:00:50 -0500

    add search function

--- firebase/search.ts
+++ firebase/search.ts
@@ -1,5 +1,14 @@
-export default function textSearch(items: string[], query: string) {
-  return items.filter((item) =>
-    item.toLowerCase().includes(query.toLowerCase())
-  );
+function search(data: string[], query: string) {
+  const queryWords = query.toLowerCase().split(" ");
+  const ranked = data.map((item) => {
+    const words = item.toLowerCase().split(" ");
+    const matches = words.filter((word) => queryWords.includes(word));
+    return {
+      item,
+      matches,
+      rank: matches.length,
+    };
+  });
+  ranked.sort((a, b) => b.rank - a.rank);
+  return ranked.map((item) => item.item);
 }
