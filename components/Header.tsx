3651a0c794dad035d9fba2c5fe899b9b49a78f44
firesquid <jdeiss06@gmail.com>
2023-07-26 13:52:21 -0500

    add favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import AuthButton from "./AuthButton";
 
 export default async function Header() {
@@ -6,6 +7,7 @@ export default async function Header() {
     <header className="navbar sticky top-0 bg-base-200">
       <div className="flex-1">
         <Link href="/dashboard" className="btn btn-ghost normal-case text-xl">
+          <Image src="/icon.png" alt="icon" width={50} height={50} />
           Cyberbook
         </Link>
       </div>
